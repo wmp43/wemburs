81960eff3d47f60c822aff797afb6133bee85e24
wmp43 <wmp43@cornell.edu>
2023-11-27 17:30:00 -0500

    unsure

---
+++ setup.py
@@ -0,0 +1,12 @@
+# from wemburs.src.descriptive_statistics import mean
+# import numpy as np
+#
+# arr = np.array([1.2, 3.1, 5.6, 9.2, 5.4, 7.5, 1.1, 0.3, 4.0])
+#
+# mean = mean(arr)
+#
+# print(mean)
+
+import wemburs
+
+
